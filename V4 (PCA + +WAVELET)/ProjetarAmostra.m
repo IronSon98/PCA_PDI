2311cbf616f2ac92370e3b31766ddabef0d082a5
pedrinnh <pedrinnh12@gmail.com>
2020-06-05 02:19:14 -0300

    mudança

---
+++ V4 (PCA + +WAVELET)/ProjetarAmostra.m
@@ -0,0 +1,17 @@
+%Projeta uma nova amostra no espaço vetorial P
+%Entrada:
+%   x = amostra a ser projetada
+%   mn = média de cada coluna da matriz de dados
+%   P = dados no novo espaço vetorial (autofaces no caso de imagens)
+%Saída:
+%   x = amostra no novo espaço vetorial
+function features = ProjetarAmostra(x,mn,P,sn)
+    img_test = imresize(x, [256 256]);
+    Feature1 = featureMatrix(sn, img_test);
+    Feature1 = mean(mean(Feature1,2),3);
+    %img_test = reshape(img_test,[size(img_test,1)*size(img_test,2),1]);
+%     LBP = extractLBPFeatures(img_test, 'Upright' , false);
+    %features = reshape(features, [size(features,1)*size(features,2), 1]);
+    features = double(Feature1) - mn;
+    features = P' * features;
+end
